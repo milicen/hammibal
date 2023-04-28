308c30733e19b6ed55db5fcdd6d339e6f873264a
milicen <clsta.lee@gmail.com>
2023-04-28 11:21:25 +0700

    Custom YSort hamsters by mass

--- multiplayer_client/scenes/main.gd
+++ multiplayer_client/scenes/main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+var players = []
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Server.connect_to_server()
@@ -10,3 +13,43 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_player_spawner_spawned(node):
+	players.append(node)
+
+
+func _on_player_spawner_despawned(node):
+	var player = players.find(node)
+	players.erase(player)
+
+
+func _on_player_order_timer_timeout():
+	# mass sort
+	var copy = players.duplicate()
+	for n in copy.size():
+		var index = 0
+		
+		for m in copy.size():
+			var current = copy[index]
+			if index+1 > copy.size()-1: break
+			var next = copy[index+1]
+			
+			if current == null or next == null: break
+		
+			if current.mass > next.mass:
+				var tmp = current
+				copy[index] = next
+				copy[index+1] = tmp
+			
+			index += 1
+	
+	players = copy
+	for index in players.size():
+		if players[index] == null: continue
+		players[index].z_index = index
+		
+	
+
+
+
