aa7135835732f1ec05fb04fa3261709079277f35
milicen <clsta.lee@gmail.com>
2023-05-09 19:12:49 +0700

    Optimize toyball to kill small hamsters only Bounce toyball when bumping on bigger hamsters

--- multiplayer_client/scenes/objects/toy_ball.gd
+++ multiplayer_client/scenes/objects/toy_ball.gd
@@ -5,15 +5,24 @@ extends CharacterBody2D
 	set(value):
 		is_moving = value
 		if value:
-			set_collision_mask_value(2, false)
+			print('moving')
+#			set_collision_mask_value(2, false)
+			set_collision_layer_value(3, false)
 #			$CollisionShape2D.disabled = true
 			$HamsterArea.monitoring = true
 		else:
-			set_collision_mask_value(2, true)
+			print('not moving')
+#			set_collision_mask_value(2, true)
+			set_collision_layer_value(3, true)
 #			$CollisionShape2D.disabled = false
 			$HamsterArea.monitoring = false
 
 
 func _on_hamster_area_body_entered(body):
-	$Squish.play()
-	Game.request_kill_hamster(body.name)
+	if body.scale.x <= 0.5:
+		$Squish.play()
+		Game.request_kill_hamster(body.name)
+	else:
+		print('move ball')
+		Game.rpc('move_toy_ball', self.name, force.bounce(body.global_position.direction_to(global_position)))
+#		Game.move_toy_ball(self.name, force.bounce(body.global_position.direction_to(global_position)))
